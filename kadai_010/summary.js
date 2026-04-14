0d060c7f9c2a316d72961211f9b40357117af14e
yuto-taira-pixel <yuto-taira@gr.paramix.co.jp>
2026-04-14 16:31:40 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,18 @@
+$(function() {
+ // ボタンがクリックされた場合
+    $('#change-color').on('click', function(){
+    $('#target').css('color', 'red');
+    });
+
+    $('#change-text').on('click', function(){
+    $('#target').text('Hello!');
+    });
+
+    $('#fade-out').on('click', function(){
+    $('#target').fadeOut();
+    });
+
+    $('#fade-in').on('click', function(){
+    $('#target').fadeIn();
+    });
+});
